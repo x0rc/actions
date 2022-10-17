98c8243f2e559788906f2e8f9d37d810a479fc8d
x0rc <x0rc@gmail.com>
2022-10-18 01:30:49 +0200

    week01

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
   # Ireland
-  default     = "us-east-1"
+  default     = "eu-west-1"
 }
 # Use the command line to inject this variable
 variable "personal_access_token"{
   description = "personal token"
-  default = "personal_access_token"
+  default = "replace_this_with_your_token"
 }
